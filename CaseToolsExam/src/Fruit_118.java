9a8685133030dbba66a0956ef0871ca0e14e3ede
Berwin <Berwin@Berwin>
2021-07-23 14:17:25 +0530

    first

---
+++ CaseToolsExam/src/Fruit_118.java
@@ -0,0 +1,14 @@
+
+public class Fruit_118 {
+	String color;
+	Fruit_118(String color)
+	{
+	this.color=color;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
